f3565d12911e3b238f62a092f2ea7b8a40bd6987
Andrea Santillana <andreasantillana@gmail.com>
2016-03-10 18:15:15 -0600

    quitar folder extra

--- Alto nivel/all-out/cosas_del_roquillo/timeJ3.sh
+++
@@ -1,8 +0,0 @@
-echo 100pts
-for i in `seq 0 9`;
-do
-  echo $i
-  /usr/bin/time ./solutions/100ptsB < cases/big.$i.in | ./altval2
-done
-
-
--- Alto nivel/all-out/cosas_del_roquillo/uwot.cpp
+++
@@ -1,7 +0,0 @@
-#include<iostream>
-using namespace std;
-
-int main(){
-  cout << "u wot";
-  return 0;
-}
